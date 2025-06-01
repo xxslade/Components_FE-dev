746251ffc4e3feedc858dd6b3d52bacd40e9960c
ShrutikSingh <shrutiksingh.26@gmail.com>
2025-06-01 16:46:18 +0530

    Initial navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Sidebar from './components/Sidebar.jsx';
-
+import Navbar from './components/Navbar.jsx';
 
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <Sidebar/>
-      Hello we are starting
+    <div className='w-full h-full absolute bg-[#0F0F0F]'>
+      {/* <Sidebar/> */}
+      <Navbar/>
+      <div className='text-white flex items-center justify-center'>
+          Hello we are starting
+      </div>
+      
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { IoSearch } from "react-icons/io5";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="flex flex-row justify-between items-center py-4 px-8 bg-[#513B3C] rounded mt-[10px] ml-[20px] mr-[20px]">
+        <div className="text-xl font-bold text-red-200 ml-[20px]">
+          COMPONENTS
+        </div>
+
+        <div className="flex items-center gap-2 border px-2 py-1 rounded">
+          <input
+            type="text"
+            placeholder='Search'
+            className="outline-none placeholder-white"
+          />
+          <IoSearch />
+        </div>
+
+        <div className='flex flex-row gap-8 mr-[80px]'>
+          <span className='text-white'>Home</span>
+          <span className='text-white'>Explore</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
