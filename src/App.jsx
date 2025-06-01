4ea3cffc55c504994b4fc29b540bc7dccfec8edf
Mahtab <mahtab2507@gmail.com>
2025-06-01 21:20:30 +0530

    sidebar modified to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import './App.css'
 function App() {
   return (
     <div className='w-full h-full absolute bg-[#0F0F0F]'>
-      {/* <Sidebar/> */}
       <Navbar/>
+      <Sidebar/>
+    
       <div className='text-white flex items-center justify-center'>
           Hello we are starting
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,43 @@
-import './Sidebar.css';
 import { NavLink, Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
-    <div className="outer-box">
-      <div className="heading">
+    <div className="m-0 bg-[#1e293b] w-[20vw] h-screen text-[bisque] font-['Franklin_Gothic_Medium','Arial_Narrow','Arial',sans-serif]">
+      <div className="font-['Segoe_UI','Tahoma','Geneva','Verdana',sans-serif] text-lg flex items-center justify-center h-[10%] text-white">
         Get started with your element!
       </div>
-      <div className="elements">
-        <NavLink to="/" className={({ isActive }) => isActive ? 'indi-elements active' : 'indi-elements'}>Buttons</NavLink>
-        <NavLink to="/navbars" className={({ isActive }) => isActive ? 'indi-elements active' : 'indi-elements'}>Navbars</NavLink>
-        <NavLink to="/cards" className={({ isActive }) => isActive ? 'indi-elements active' : 'indi-elements'}>Cards</NavLink>
+      <div className="flex flex-col justify-evenly items-center h-[25%]">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `text-white no-underline w-full h-full ${isActive ? "bg-[#516591]" : "bg-[#233753]"
+            } flex justify-center items-center text-lg hover:bg-[#475569]`
+          }
+        >
+          Buttons
+        </NavLink>
+
+        <NavLink
+          to="/navbars"
+          className={({ isActive }) =>
+            `text-white no-underline w-full h-full ${isActive ? "bg-[#516591]" : "bg-[#233753]"
+            } flex justify-center items-center text-lg hover:bg-[#475569]`
+          }
+        >
+          Navbars
+        </NavLink>
+
+        <NavLink
+          to="/cards"
+          className={({ isActive }) =>
+            `text-white no-underline w-full h-full ${isActive ? "bg-[#516591]" : "bg-[#233753]"
+            } flex justify-center items-center text-lg hover:bg-[#475569]`
+          }
+        >
+          Cards
+        </NavLink>
+
+
       </div>
     </div>
   );
