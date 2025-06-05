e15f4e3993f8fb1440a1adc2b0dad2761a9acd75
Mahtab <mahtab2507@gmail.com>
2025-06-05 18:20:42 +0530

    added chatbox

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage.jsx';
 import CardPage from './pages/CardPage.jsx';
 import NavPage from './pages/NavPage.jsx';
 import ButtonPage from './pages/ButtonPage.jsx';
+import ChatBox from './components/ChatBox.jsx';
 import './App.css';
 
 
@@ -12,21 +13,22 @@ function App() {
   return (
     <Router>
       <div className='w-full h-screen flex flex-col bg-[#dfd9d9]'>
-       
+
         <Navbar />
 
         <div className='flex-1 flex'>
           <Sidebar />
-        
-        <div className='flex-1 p-4'>
-        <Routes>
-        <Route path="/" element={<HomePage />} />
-          <Route path="/buttons" element={<ButtonPage />} />
-          <Route path="/cards" element={<CardPage/>} />
-          <Route path="/navbars" element={<NavPage/>} />
-        </Routes>
+
+          <div className='flex-1 p-4'>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/buttons" element={<ButtonPage />} />
+              <Route path="/cards" element={<CardPage />} />
+              <Route path="/navbars" element={<NavPage />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+        <ChatBox />
       </div>
     </Router>
   );
